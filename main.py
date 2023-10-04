392c58238bdc73e7a1f8619137121e93127abe56
WanjikuN <cikunjoroge4@gmail.com>
2023-10-04 10:14:30 +0300

    refactor: define relationships and set classes

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ engine = create_engine('sqlite:///bloodhub.db')
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
+# Donation - Donor -> one-to-many
+# Donation - Hospital -> one-to-many
+# Donation - BloodReceiver  -> one-to-many
+# BloodReceiver - Hospital  -> one-to-many
+# Donor - Hospital -> many-to-many
+
+#Classes
+class Donor(Base):
+    pass
+class Donation(Base):
+    pass
+class Hospital(Base):
+    pass
+class BloodReceiver(Base):
+    pass

caf60389f8df13d120e3545d4386fb8a38cd2586
WanjikuN <cikunjoroge4@gmail.com>
2023-10-04 11:32:06 +0300

    feat: establish one to many relationships

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine,Column,Integer,String,ForeignKey,Table,Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 from datetime import datetime
- 
+
 engine = create_engine('sqlite:///bloodhub.db')
 
 Base = declarative_base()
@@ -28,12 +28,23 @@ class Donor(Base):
     donor_name = Column(String())
     blood_type = Column(String())
 
+    # relationships
+    donation = relationship("Donation", back_populates = ('donor'))
+
 class Donation(Base):
     __tablename__ = 'donations'
 
     id = Column(Integer(), primary_key=True)
     amount = Column(Integer())
     donation_date = Column(DateTime, default=datetime.now())
+    donor_id = Column(Integer(), ForeignKey('donors.id'))
+    hospital_id = Column(Integer(), ForeignKey('hospitals.id'))
+    bloodreceiver_id = Column(Integer(), ForeignKey('recepients.id'))
+
+     # relationships
+    donor = relationship("Donor", back_populates = ('donation'))
+    hospital = relationship("Hospital" , back_populates = ('donation'))
+    blood_receiver = relationship("BloodReceiver", back_populates = ('donation'))
 
     
 class Hospital(Base):
@@ -42,9 +53,14 @@ class Hospital(Base):
     id = Column(Integer(), primary_key = True)
     hospital_name = Column(String())
 
+     # relationships
+    donation = relationship("Donation", back_populates = ('hospital'))
+    blood_receiver = relationship("BloodReceiver", back_populates = ('hospital'))
+
+
 
 class BloodReceiver(Base):
-    __tablename__ = 'recepints'
+    __tablename__ = 'recepients'
     __table_args__ =(
         CheckConstraint(
         "blood_type IN ('A', 'B', 'AB', 'O')",
@@ -54,3 +70,8 @@ class BloodReceiver(Base):
     id = column(Integer(), primary_key =True)
     recepient_name = Column(String())
     blood_type = Column(String())
+    hospital_id = Column(String(), ForeignKey("hospitals.id"))
+
+    # relationships
+    donation = relationship("Donation", ForeignKey("blood_receiver"))
+    hospital = relationship("Hospital", back_populates = ('blood_receiver'))
